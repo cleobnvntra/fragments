3cdfa5fa11f033fdaa3b6a1db354ef7cd93ed5a1
cleobnvntra <97420565+cleobnvntra@users.noreply.github.com>
2023-06-07 00:28:31 -0400

    added in-memory db, updated GET request handler

---
+++ src/model/fragment.js
@@ -0,0 +1,149 @@
+// Use https://www.npmjs.com/package/content-type to create/parse Content-Type headers
+const contentType = require('content-type');
+
+// Functions for working with fragment metadata/data using our DB
+const {
+  readFragment,
+  writeFragment,
+  readFragmentData,
+  writeFragmentData,
+  listFragments,
+  deleteFragment,
+} = require('./data/memory');
+
+class Fragment {
+  constructor({ id, ownerId, created, updated, type, size = 0 }) {
+    this.id = id;
+    this.ownerId = ownerId;
+    this.created = new Date(created);
+    this.updated = new Date(updated);
+    this.type = type;
+    this.size = size;
+  }
+
+  /**
+   * Get all fragments (id or full) for the given user
+   * @param {string} ownerId user's hashed email
+   * @param {boolean} expand whether to expand ids to full fragments
+   * @returns Promise<Array<Fragment>>
+   */
+  static async byUser(ownerId, expand = false) {
+    const fragments = await listFragments(ownerId, expand);
+    const fragmentObjects = [];
+
+    for (const fragmentId of fragments) {
+      const fragment = await readFragment(ownerId, fragmentId);
+      fragmentObjects.push(new Fragment(fragment));
+    }
+
+    return Promise.resolve(fragmentObjects);
+  }
+
+  /**
+   * Gets a fragment for the user by the given id.
+   * @param {string} ownerId user's hashed email
+   * @param {string} id fragment's id
+   * @returns Promise<Fragment>
+   */
+  static async byId(ownerId, id) {
+    const fragmentData = await readFragment(ownerId, id);
+
+    if (!fragmentData) {
+      return null; // Return null if the fragment doesn't exist
+    }
+
+    return Promise.resolve(new Fragment(fragmentData));
+  }
+
+  /**
+   * Delete the user's fragment data and metadata for the given id
+   * @param {string} ownerId user's hashed email
+   * @param {string} id fragment's id
+   * @returns Promise<void>
+   */
+  static delete(ownerId, id) {
+    return Promise.resolve(deleteFragment(ownerId, id));
+  }
+
+  /**
+   * Saves the current fragment to the database
+   * @returns Promise<void>
+   */
+  save() {
+    const fragmentData = {
+      id: this.id,
+      ownerId: this.ownerId,
+      created: this.created,
+      updated: this.updated,
+      type: this.type,
+      size: this.size,
+    };
+
+    return Promise.resolve(writeFragment(fragmentData));
+  }
+
+  /**
+   * Gets the fragment's data from the database
+   * @returns Promise<Buffer>
+   */
+  async getData() {
+    return Promise.resolve(readFragmentData(this.ownerId, this.id));
+  }
+
+  /**
+   * Set's the fragment's data in the database
+   * @param {Buffer} data
+   * @returns Promise<void>
+   */
+  async setData(data) {
+    return Promise.resolve(writeFragmentData(this.ownerId, this.id, data));
+  }
+
+  /**
+   * Returns the mime type (e.g., without encoding) for the fragment's type:
+   * "text/html; charset=utf-8" -> "text/html"
+   * @returns {string} fragment's mime type (without encoding)
+   */
+  get mimeType() {
+    const { type } = contentType.parse(this.type);
+    return type;
+  }
+
+  /**
+   * Returns true if this fragment is a text/* mime type
+   * @returns {boolean} true if fragment's type is text/*
+   */
+  get isText() {
+    const { type } = contentType.parse(this.type);
+    return type.startsWith('text/');
+  }
+
+  /**
+   * Returns the formats into which this fragment type can be converted
+   * @returns {Array<string>} list of supported mime types
+   */
+  get formats() {
+    const mimeType = this.mimeType;
+    return [mimeType];
+  }
+
+  /**
+   * Returns true if we know how to work with this content type
+   * @param {string} value a Content-Type value (e.g., 'text/plain' or 'text/plain: charset=utf-8')
+   * @returns {boolean} true if we support this Content-Type (i.e., type/subtype)
+   */
+  static isSupportedType(value) {
+    const supportedTypes = [
+      'text/plain',
+      'text/markdown',
+      'text/html',
+      'application/json',
+      'image/png',
+      'image/jpeg',
+      'image/webp',
+    ];
+    return supportedTypes.includes(value);
+  }
+}
+
+module.exports.Fragment = Fragment;
--- src/routes/api/post.js
+++ src/routes/api/post.js
@@ -1,62 +1,62 @@
 const crypto = require('crypto');
 const jwt = require('jsonwebtoken');
 const { createSuccessResponse } = require('../../response');
+const { Fragment } = require('../../model/fragment');
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
   const hashEmail = (email) => {
     const hash = crypto.createHash('sha256');
     hash.update(email);
     return hash.digest('hex');
   };
 
-  // Extract the file data from the request body
-  const content = req.body;
-  const userToken = req.headers.authorization.replace('Bearer ', '');
-  const decodedToken = jwt.decode(userToken);
+  try {
+    // Extract the file data from the request body
+    const content = req.body;
+    const userToken = req.headers.authorization.replace('Bearer ', '');
+    const decodedToken = jwt.decode(userToken);
 
-  // Extract the Content-Type header from the request
-  const contentType = req.get('Content-Type');
+    // Extract the Content-Type header from the request
+    const contentType = req.get('Content-Type');
 
-  // Check if the Content-Type is supported
-  const supportedTypes = [
-    'text/plain',
-    'text/markdown',
-    'text/html',
-    'application/json',
-    'image/png',
-    'image/jpeg',
-    'image/webp',
-  ];
+    // Generate a new fragment ID using a UUID
+    const fragmentId = crypto.randomUUID();
 
-  if (!supportedTypes.includes(contentType)) {
-    return res.status(415).json({ error: 'Unsupported Media Type' });
+    // Generate the ownerId by hashing the username
+    const ownerId = hashEmail(decodedToken.email);
+
+    // Get the current timestamp for created and updated fields
+    const timestamp = new Date().toISOString();
+
+    // Calculate the size of the file data in bytes
+    const size = Buffer.byteLength(content);
+
+    // Create a new instance of the Fragment class
+    const fragmentData = {
+      id: fragmentId,
+      ownerId: ownerId,
+      created: timestamp,
+      updated: timestamp,
+      type: contentType,
+      size: size,
+    };
+
+    const newFragment = await new Fragment(fragmentData);
+
+    // Check if the Content-Type is supported
+    if (!Fragment.isSupportedType(contentType)) {
+      return res.status(415).json({ error: 'Unsupported Media Type' });
+    }
+
+    // Store the file data and metadata in the database or any other storage mechanism
+    await newFragment.setData(content);
+    await newFragment.save();
+
+    // Send the response with the newly created fragment metadata
+    const data = createSuccessResponse({ fragment: newFragment });
+    res.status(201).json(data);
+  } catch (error) {
+    console.error('Error creating fragment:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
-
-  // Generate a new fragment ID using a UUID
-  const fragmentId = crypto.randomUUID();
-
-  // Generate the ownerId by hashing the username
-  const ownerId = hashEmail(decodedToken.email);
-
-  // Get the current timestamp for created and updated fields
-  const timestamp = new Date().toISOString();
-
-  // Calculate the size of the file data in bytes
-  const size = Buffer.byteLength(content);
-
-  // Create the new fragment metadata
-  const newFragment = {
-    id: fragmentId,
-    ownerId: ownerId,
-    created: timestamp,
-    updated: timestamp,
-    type: contentType,
-    size: size,
-  };
-
-  // Store the file data and metadata in the database or any other storage mechanism
-
-  const data = createSuccessResponse({ fragment: newFragment });
-  // Send the response with the newly created fragment metadata
-  res.status(201).location().json(data);
 };
