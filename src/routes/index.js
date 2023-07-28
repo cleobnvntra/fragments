5dd0ae733a40017dd12127aeb0b6625006702eee
cleobnvntra <97420565+cleobnvntra@users.noreply.github.com>
2023-07-28 00:17:31 -0400

    added os.hostname

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,8 +11,10 @@ const { authenticate } = require('../authorization/index');
 // Create a router that we can use to mount our API
 const router = express.Router();
 
+const { hostname } = require('os');
+
 // response creator from response.js
-const {createSuccessResponse} = require('../response');
+const { createSuccessResponse } = require('../response');
 
 /**
  * Expose all of our API routes on /v1/* to include an API version.
@@ -26,12 +28,12 @@ router.use(`/v1`, authenticate(), require('./api'));
 router.get('/', (req, res) => {
   // Client's shouldn't cache this response (always request it fresh)
   res.setHeader('Cache-Control', 'no-cache');
-  const data = createSuccessResponse(
-    {
-      author,
-      githubUrl: 'https://github.com/cleobnvntra/fragments',
-      version
-    });
+  const data = createSuccessResponse({
+    author,
+    githubUrl: 'https://github.com/cleobnvntra/fragments',
+    version,
+    hostname: hostname(),
+  });
   // Send a 200 'OK' response
   res.status(200).json(data);
 });
